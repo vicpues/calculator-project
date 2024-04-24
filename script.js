3cfa7e98a0349901c7f7f83c36c8728e03386da9
Vic Pues <141432283+vicpues@users.noreply.github.com>
2024-04-24 21:10:15 +0200

    feat: Add operate() function

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
 function add(a, b) {
-    return a + b
+    return a + b;
 }
 
 
 function subtract(a, b) {
-    return a - b
+    return a - b;
 }
 
 
 function multiply(a, b) {
-    return a * b
+    return a * b;
 }
 
 
 function divide(a, b) {
-    return a / b
+    return a / b;
+}
+
+
+function operate(firstNum, operator, secondNum) {
+
+    switch (operator) {
+        case "+":
+            return add(firstNum, secondNum);
+        case "-":
+            return subtract(firstNum, secondNum);
+        case "*":
+            return add(firstNum, secondNum);
+        case "/":
+            return add(firstNum, secondNum);   
+        default:
+            console.log("Error: either the operator or the numbers are not valid");
+    };
 }
 
 
 const firstNum = 3;
 const secondNum = 4;
 const operator = "+";
-
