e941fcc0b3bd813ba464d20c66318f86ebcd696f
Vic Pues <141432283+vicpues@users.noreply.github.com>
2024-04-25 07:54:05 +0200

    feat: Add calculator logic

--- script.js
+++ script.js
@@ -35,6 +35,89 @@ function operate(firstNum, operator, secondNum) {
 }
 
 
-const firstNum = 3;
-const secondNum = 4;
-const operator = "+";
+const buttonMap = {
+    "one-button":       {emits:"1",  type:"number"},
+    "two-button":       {emits:"2",  type:"number"},
+    "three-button":     {emits:"3",  type:"number"},
+    "four-button":      {emits:"4",  type:"number"},
+    "five-button":      {emits:"5",  type:"number"},
+    "six-button":       {emits:"6",  type:"number"},
+    "seven-button":     {emits:"7",  type:"number"},
+    "eight-button":     {emits:"8",  type:"number"},
+    "nine-button":      {emits:"9",  type:"number"},
+    "zero-button":      {emits:"0",  type:"number"},
+    "dot-button":       {emits:".",  type:"number"},
+    "add-button":       {emits:"+",  type:"operator"},
+    "subtract-button":  {emits:"-",  type:"operator"},
+    "multiply-button":  {emits:"*",  type:"operator"},
+    "divide-button":    {emits:"/",  type:"operator"},
+    "backspace-button": {emits:null, type:"backspace"},
+    "clear-button":     {emits:null, type:"clear"},
+    "equals-button":    {emits:"=",  type:"equals"},
+};
+
+const buttonPad = document.querySelector("#button-pad");
+buttonPad.addEventListener("click", (e) => clickHandler(e));
+
+
+function clickHandler(e) {
+
+    // Handle clicks outside of a button
+    let button = (e.target.tagName === "BUTTON")
+        ? buttonMap[e.target.id]
+        : null;
+    if (button === null) {return};
+
+    // Choose which behaviour to activate
+    updateOperation()
+    switch (button.type) {
+        
+        case "number":
+            if (operation.hasEquals) {
+                clearScreens();
+            }
+            lowerScreen.textContent += button.emits;
+            break;
+
+        case "operator":
+
+            upperScreen.textContent = `${lowerScreen.textContent} ${button.emits}`;
+            lowerScreen.textContent = "";
+            break;
+
+        case "backspace":
+            lowerScreen.textContent = lowerScreen.textContent.slice(0, -1);
+            break;
+
+        case "clear":
+            clearScreens();
+            break;
+
+        case "equals":
+            upperScreen.textContent = `${operation.firstNum} ${operation.operator} ${operation.secondNum} ${button.emits}`
+            lowerScreen.textContent = operate(operation.firstNum, operation.operator, operation.secondNum);
+            break;
+    };
+}
+
+
+function clearScreens() {
+    upperScreen.textContent = "";
+    lowerScreen.textContent = "";
+}
+
+
+function updateOperation() {
+    let upperScreenArr = upperScreen.textContent.split(" ");
+    return {
+        firstNum: Number(upperScreenArr[0]),
+        operator: upperScreenArr[1],
+        secondNum: Number(lowerScreen.textContent),
+        hasEquals: (upperScreenArr.slice(-1)[0] === "="),
+    };
+}
+
+
+const upperScreen = document.querySelector("#upper-display");
+const lowerScreen = document.querySelector("#lower-display");
+let operation;
