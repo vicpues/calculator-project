b712cf976481f1a626fce7f2f431431eeed909b2
Vic Pues <141432283+vicpues@users.noreply.github.com>
2024-04-26 03:41:06 +0200

    refactor: Reorder functions more logically

--- script.js
+++ script.js
@@ -1,73 +1,10 @@
-function add(a, b) {
-    return a + b;
-}
-
-
-function subtract(a, b) {
-    return a - b;
-}
-
-
-function multiply(a, b) {
-    return a * b;
-}
-
-
-function divide(a, b) {
-    return a / b;
-}
-
-
-function operate(firstNum, operator, secondNum) {
-
-    switch (operator) {
-        case "+":
-            return add(firstNum, secondNum);
-        case "-":
-            return subtract(firstNum, secondNum);
-        case "*":
-            return multiply(firstNum, secondNum);
-        case "/":
-            return divide(firstNum, secondNum);   
-        default:
-            console.log("Error: either the operator or the numbers are not valid");
-    };
-}
-
-
-const buttonMap = {
-    "one-button":       {emits:"1",  type:"number"   },
-    "two-button":       {emits:"2",  type:"number"   },
-    "three-button":     {emits:"3",  type:"number"   },
-    "four-button":      {emits:"4",  type:"number"   },
-    "five-button":      {emits:"5",  type:"number"   },
-    "six-button":       {emits:"6",  type:"number"   },
-    "seven-button":     {emits:"7",  type:"number"   },
-    "eight-button":     {emits:"8",  type:"number"   },
-    "nine-button":      {emits:"9",  type:"number"   },
-    "zero-button":      {emits:"0",  type:"number"   },
-    "dot-button":       {emits:".",  type:"number"   },
-    "add-button":       {emits:"+",  type:"operator" },
-    "subtract-button":  {emits:"-",  type:"operator" },
-    "multiply-button":  {emits:"*",  type:"operator" },
-    "divide-button":    {emits:"/",  type:"operator" },
-    "backspace-button": {emits:null, type:"backspace"},
-    "clear-button":     {emits:null, type:"clear"    },
-    "equals-button":    {emits:null, type:"equals"   },
-};
-
-const buttonPad = document.querySelector("#button-pad");
-buttonPad.addEventListener("click", (e) => clickHandler(e));
-
-
 function clickHandler(e) {
-
     // Handle clicks outside of a button
     let button = (e.target.tagName === "BUTTON")
         ? buttonMap[e.target.id]
         : null;
     if (button === null) {return};
-
+    
     // Choose which behaviour to activate
     switch (button.type) {
         
@@ -78,29 +15,29 @@ function clickHandler(e) {
         case "operator":
             operatorInput(button.emits);
             break;
-
+            
         case "backspace":
             backspaceInput();
             break;
-
+            
         case "clear":
             clearScreens();
             break;
 
-        case "equals":
-            equalsInput();
-            break;
-    };
-
-    updateScreens();
-}
-
-
+            case "equals":
+                equalsInput();
+                break;
+            };
+            
+            updateScreens();
+        }
+        
+        
 function numberInput(number) {
     if (operation.result) {
         clearScreens();
     };
-
+    
     if (number === ".") {
         if (!readLowerScreen()) {
             lowerScreen.textContent += "0.";
@@ -154,9 +91,7 @@ function backspaceInput() {
 
 
 function equalsInput() {
-    if (
-        operation.operator === "/" && 
-        parseLowerScreen() == "0") {
+    if (operation.operator === "/" && parseLowerScreen() == "0") {
         alert("The universe self-destructs... :(")
     } else if (
         operation.firstNum &&
@@ -181,16 +116,6 @@ function clearScreens() {
 
 function clearLowerScreen() {
     lowerScreen.textContent = ""
-}
-
-
-function resetOperation() {
-    operation = {
-        firstNum: null,
-        operator: null,
-        secondNum: null,
-        result: null,
-    };
 }
 
 
@@ -231,6 +156,16 @@ function updateOperation(obj) {
 }
 
 
+function resetOperation() {
+    operation = {
+        firstNum: null,
+        operator: null,
+        secondNum: null,
+        result: null,
+    };
+}
+
+
 function computeResult() {
     operation.result = operate(
         operation.firstNum,
@@ -239,6 +174,64 @@ function computeResult() {
     );
 }
 
+
+function operate(firstNum, operator, secondNum) {
+
+    switch (operator) {
+        case "+":
+            return add(firstNum, secondNum);
+        case "-":
+            return subtract(firstNum, secondNum);
+        case "*":
+            return multiply(firstNum, secondNum);
+        case "/":
+            return divide(firstNum, secondNum);   
+        default:
+            console.log("Error: either the operator or the numbers are not valid");
+    };
+}
+
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+
+const buttonMap = {
+    "one-button":       {emits:"1",  type:"number"   },
+    "two-button":       {emits:"2",  type:"number"   },
+    "three-button":     {emits:"3",  type:"number"   },
+    "four-button":      {emits:"4",  type:"number"   },
+    "five-button":      {emits:"5",  type:"number"   },
+    "six-button":       {emits:"6",  type:"number"   },
+    "seven-button":     {emits:"7",  type:"number"   },
+    "eight-button":     {emits:"8",  type:"number"   },
+    "nine-button":      {emits:"9",  type:"number"   },
+    "zero-button":      {emits:"0",  type:"number"   },
+    "dot-button":       {emits:".",  type:"number"   },
+    "add-button":       {emits:"+",  type:"operator" },
+    "subtract-button":  {emits:"-",  type:"operator" },
+    "multiply-button":  {emits:"*",  type:"operator" },
+    "divide-button":    {emits:"/",  type:"operator" },
+    "backspace-button": {emits:null, type:"backspace"},
+    "clear-button":     {emits:null, type:"clear"    },
+    "equals-button":    {emits:null, type:"equals"   },
+};
+
+const buttonPad = document.querySelector("#button-pad");
+buttonPad.addEventListener("click", (e) => clickHandler(e));
 
 const upperScreen = document.querySelector("#upper-display");
 const lowerScreen = document.querySelector("#lower-display");
